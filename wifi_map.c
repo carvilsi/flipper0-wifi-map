98eaf017d981bc04467406d9f00c7e5923ede5f0
carvilsi <carvilsi.mail.list@gmail.com>
2023-06-17 19:47:11 +0200

    trying to draw APs, but nothing working at all :(

--- wifi_map.c
+++ wifi_map.c
@@ -18,43 +18,22 @@
 #define COLUMNS_ON_SCREEN 21
 #define WORKER_EVENTS_MASK (WorkerEventStop | WorkerEventRx)
 
-typedef struct UartDumpModel UartDumpModel;
 
 typedef struct {
     Gui* gui;
-    NotificationApp* notification;
-    ViewDispatcher* view_dispatcher;
-    View* view;
+    ViewPort* view_port;
     FuriThread* worker_thread;
     FuriStreamBuffer* rx_stream;
     File* file;
+    FuriMessageQueue* event_queue;
+    char c;
 } WiFiMapApp;
-
-typedef struct {
-    FuriString* text;
-} ListElement;
-
-struct UartDumpModel {
-    ListElement* list[LINES_ON_SCREEN];
-    uint8_t line;
-
-    char last_char;
-    bool escape;
-    File* file;
-};
 
 typedef enum {
     WorkerEventReserved = (1 << 0), // Reserved for StreamBuffer internal event
     WorkerEventStop = (1 << 1),
     WorkerEventRx = (1 << 2),
 } WorkerEventFlags;
-
-const NotificationSequence sequence_notification = {
-    &message_display_backlight_on,
-    &message_green_255,
-    &message_delay_10,
-    NULL,
-};
 
 File* open_file(){
     Storage* storage = furi_record_open(RECORD_STORAGE);
@@ -69,7 +48,7 @@ File* open_file(){
 int32_t write_to_file(char data_line, File *file) {
     char *data = (char *)malloc(sizeof(char) + 1);
     data[0] = data_line;
-    if(!storage_file_write(file, data, (uint16_t)strlen(data))) {
+    if (!storage_file_write(file, data, (uint16_t)strlen(data))) {
         FURI_LOG_E(TAG, "Failed to write to file");
     }
     free(data);
@@ -83,44 +62,20 @@ int32_t close_file(File *file) {
     return 0;
 }
 
-static void uart_echo_view_draw_callback(Canvas* canvas, void* _model) {
-    UartDumpModel* model = _model;
-
+static void wifi_map_view_draw_callback(Canvas* canvas, void* context) {
+    WiFiMapApp *app = context;
+    FURI_LOG_D(TAG, "%p", app->view_port);
     // Prepare canvas
+    char *lol = "this";
     canvas_clear(canvas);
     canvas_set_color(canvas, ColorBlack);
     canvas_set_font(canvas, FontKeyboard);
-
-    for(size_t i = 0; i < LINES_ON_SCREEN; i++) {
-        canvas_draw_str(
-            canvas,
-            0,
-            (i + 1) * (canvas_current_font_height(canvas) - 1),
-            furi_string_get_cstr(model->list[i]->text));
-
-        if(i == model->line) {
-            uint8_t width =
-                canvas_string_width(canvas, furi_string_get_cstr(model->list[i]->text));
-
-            canvas_draw_box(
-                canvas,
-                width,
-                (i) * (canvas_current_font_height(canvas) - 1) + 2,
-                2,
-                canvas_current_font_height(canvas) - 2);
-        }
-    }
+    canvas_draw_str(canvas, 10, 10, lol);
 }
 
-static bool uart_echo_view_input_callback(InputEvent* event, void* context) {
-    UNUSED(event);
-    UNUSED(context);
-    return false;
-}
-
-static uint32_t uart_echo_exit(void* context) {
-    UNUSED(context);
-    return VIEW_NONE;
+static void wifi_map_view_input_callback(InputEvent* event, void* context) {
+    WiFiMapApp* app = context;
+    furi_message_queue_put(app->event_queue, event, FuriWaitForever);
 }
 
 static void uart_echo_on_irq_cb(UartIrqEvent ev, uint8_t data, void* context) {
@@ -133,126 +88,65 @@ static void uart_echo_on_irq_cb(UartIrqEvent ev, uint8_t data, void* context) {
     }
 }
 
-static void uart_echo_push_to_list(UartDumpModel* model, const char data , WiFiMapApp* app) {
-    if(model->escape) {
-        // escape code end with letter
-        if((data >= 'a' && data <= 'z') || (data >= 'A' && data <= 'Z')) {
-            model->escape = false;
-        }
-    } else if(data == '[' && model->last_char == '\e') {
-        // "Esc[" is a escape code
-        model->escape = true;
-    } else if((data >= ' ' && data <= '~') || (data == '\n' || data == '\r')) {
+static void uart_echo_push_to_list(const char data , WiFiMapApp* app) {
+    if((data >= ' ' && data <= '~') || (data == '\n' || data == '\r')) {
         write_to_file((char) data,  app->file);
-        bool new_string_needed = false;
-        if(furi_string_size(model->list[model->line]->text) >= COLUMNS_ON_SCREEN) {
-            new_string_needed = true;
-        } else if((data == '\n' || data == '\r')) {
-            // pack line breaks
-            if(model->last_char != '\n' && model->last_char != '\r') {
-                new_string_needed = true;
-            }
-        }
-
-        if(new_string_needed) {
-            if((model->line + 1) < LINES_ON_SCREEN) {
-                model->line += 1;
-            } else {
-                ListElement* first = model->list[0];
-
-                for(size_t i = 1; i < LINES_ON_SCREEN; i++) {
-                    model->list[i - 1] = model->list[i];
-                }
-
-                furi_string_reset(first->text);
-                model->list[model->line] = first;
-            }
-        }
-
-        if(data != '\n' && data != '\r') {
-            furi_string_push_back(model->list[model->line]->text, data);
-        }
+        app->c = data;
+        FURI_LOG_D(TAG, "%c", data);
     }
-    model->last_char = data;
 }
 
-static int32_t uart_echo_worker(void* context) {
+static int32_t wifi_map_worker(void* context) {
     furi_assert(context);
     WiFiMapApp* app = context;
 
-    while(1) {
-        uint32_t events =
-            furi_thread_flags_wait(WORKER_EVENTS_MASK, FuriFlagWaitAny, FuriWaitForever);
-        furi_check((events & FuriFlagError) == 0);
-
-        if(events & WorkerEventStop) break;
-        if(events & WorkerEventRx) {
-            size_t length = 0;
-            do {
-                uint8_t data[64];
-                length = furi_stream_buffer_receive(app->rx_stream, data, 64, 0);
-                if(length > 0) {
-                    furi_hal_uart_tx(FuriHalUartIdUSART1, data, length);
-                    with_view_model(
-                        app->view,
-                        UartDumpModel * model,
-                        {
-                            for(size_t i = 0; i < length; i++) {
-                                uart_echo_push_to_list(model, data[i], app);
-                            }
-                        },
-                        false);
+    for(bool is_running = true; is_running;) {
+        InputEvent event;
+        /* Wait for an input event. Input events come from the GUI thread via a callback. */
+        const FuriStatus status = furi_message_queue_get(app->event_queue, &event, FuriWaitForever);
+        
+        size_t length = 0;
+        do {
+            uint8_t data[64];
+            length = furi_stream_buffer_receive(app->rx_stream, data, 64, 0);
+            if(length > 0) {
+                furi_hal_uart_tx(FuriHalUartIdUSART1, data, length);
+                for (size_t i = 0; i < length; i++) {
+                    uart_echo_push_to_list(data[i], app);
                 }
-            } while(length > 0);
+            }
+        } while(length > 0);
 
-            notification_message(app->notification, &sequence_notification);
-            with_view_model(
-                app->view, UartDumpModel * model, { UNUSED(model); }, true);
-        }
+        /* This application is only interested in short button presses. */
+        if((status != FuriStatusOk) || (event.type != InputTypeShort))
+            continue;
+
+        /* When the user presses the "Back" button, break the loop and exit the application. */
+        if(event.key == InputKeyBack) 
+            is_running = false;
     }
 
+    FURI_LOG_D(TAG, "EXIT");
     return 0;
 }
 
-static WiFiMapApp* uart_echo_app_alloc() {
+static WiFiMapApp* wifi_map_app_alloc() {
     WiFiMapApp* app = malloc(sizeof(WiFiMapApp));
-
     app->file = open_file();
-
     app->rx_stream = furi_stream_buffer_alloc(2048, 1);
+
+    // View
+    app->view_port = view_port_alloc();
+    view_port_draw_callback_set(app->view_port, wifi_map_view_draw_callback, app);
+    view_port_input_callback_set(app->view_port, wifi_map_view_input_callback, app);
+
+    app->event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
 
     // Gui
     app->gui = furi_record_open(RECORD_GUI);
-    app->notification = furi_record_open(RECORD_NOTIFICATION);
+    gui_add_view_port(app->gui, app->view_port, GuiLayerFullscreen);
 
-    // View dispatcher
-    app->view_dispatcher = view_dispatcher_alloc();
-    view_dispatcher_enable_queue(app->view_dispatcher);
-    view_dispatcher_attach_to_gui(app->view_dispatcher, app->gui, ViewDispatcherTypeFullscreen);
-
-    // Views
-    app->view = view_alloc();
-    view_set_draw_callback(app->view, uart_echo_view_draw_callback);
-    view_set_input_callback(app->view, uart_echo_view_input_callback);
-    view_allocate_model(app->view, ViewModelTypeLocking, sizeof(UartDumpModel));
-    with_view_model(
-        app->view,
-        UartDumpModel * model,
-        {
-            for(size_t i = 0; i < LINES_ON_SCREEN; i++) {
-                model->line = 0;
-                model->escape = false;
-                model->list[i] = malloc(sizeof(ListElement));
-                model->list[i]->text = furi_string_alloc();
-            }
-        },
-        true);
-
-    view_set_previous_callback(app->view, uart_echo_exit);
-    view_dispatcher_add_view(app->view_dispatcher, 0, app->view);
-    view_dispatcher_switch_to_view(app->view_dispatcher, 0);
-
-    app->worker_thread = furi_thread_alloc_ex("UsbUartWorker", 1024, uart_echo_worker, app);
+    app->worker_thread = furi_thread_alloc_ex("WifiMapUartWorker", 1024, wifi_map_worker, app);
     furi_thread_start(app->worker_thread);
 
     // Enable uart listener
@@ -273,20 +167,9 @@ static void uart_echo_app_free(WiFiMapApp* app) {
     furi_thread_free(app->worker_thread);
 
     // Free views
-    view_dispatcher_remove_view(app->view_dispatcher, 0);
-
-    with_view_model(
-        app->view,
-        UartDumpModel * model,
-        {
-            for(size_t i = 0; i < LINES_ON_SCREEN; i++) {
-                furi_string_free(model->list[i]->text);
-                free(model->list[i]);
-            }
-        },
-        true);
-    view_free(app->view);
-    view_dispatcher_free(app->view_dispatcher);
+    view_port_enabled_set(app->view_port, false);
+    gui_remove_view_port(app->gui, app->view_port);
+    view_port_free(app->view_port);
 
     // Close gui record
     furi_record_close(RECORD_GUI);
@@ -295,6 +178,7 @@ static void uart_echo_app_free(WiFiMapApp* app) {
 
     furi_stream_buffer_free(app->rx_stream);
 
+    // closing the file
     close_file(app->file);
 
     // Free rest
@@ -303,9 +187,12 @@ static void uart_echo_app_free(WiFiMapApp* app) {
 
 int32_t wifi_map_app(void *p){
 	UNUSED(p);
-	FURI_LOG_D(TAG, "wifi_map_app");
-	WiFiMapApp* app = uart_echo_app_alloc();
-    view_dispatcher_run(app->view_dispatcher);
+	FURI_LOG_I(TAG, "wifi_map_app");
+    FURI_LOG_D(TAG, "WTF0");
+
+	WiFiMapApp* app = wifi_map_app_alloc();
+    FURI_LOG_D(TAG, "lol0");
+    wifi_map_worker(app);
     uart_echo_app_free(app);
 	return 0;
 }
